4f0316e8616a569d486c197b5f496fa57d741ebd
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-07-05 23:31:37 +0900

    Create pick.cpp

---
+++ pick.cpp
@@ -0,0 +1,15 @@
+void pick(int n, vector<int>& picked, int toPick) {
+	if (toPick == 0) {
+		for (auto i : picked)
+			cout << i << ' ';
+		return;
+	}
+
+	int smallest = picked.empty() ? 0 : picked.back() + 1;
+
+	for (int next = smallest; next < n; next++) {
+		picked.push_back(next);
+		pick(n, picked, toPick - 1);
+		picked.pop_back();
+	}
+}
