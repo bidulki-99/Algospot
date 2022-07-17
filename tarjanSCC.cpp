d9c65c71219ddbd807db372eba673f60a24f117f
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-07-18 03:00:02 +0900

    Create tarjanSCC.cpp

---
+++ tarjanSCC.cpp
@@ -0,0 +1,44 @@
+vector<vector<int>> adj;
+vector<int> sccId;
+vector<int> discovered;
+stack<int> st;
+int sccCounter, vectexCounter;
+
+int scc(int here) {
+    int ret = discovered[here] = vectexCounter++;
+    st.push(here);
+
+    for (int i = 0; i < adj[here].size(); i++) {
+        int there = adj[here][i];
+
+        if (discovered[there] == -1)
+            ret = min(ret, scc(there));
+        else if (sccId[there] == -1)
+            ret = min(ret, discovered[there]);
+    }
+
+    if (ret == discovered[here]) {
+        while (true) {
+            int t = st.top();
+            st.pop();
+            sccId[t] = sccCounter;
+            if (t == here)
+                break;
+        }
+
+        sccCounter++;
+    }
+
+    return ret;
+}
+
+vector<int> tarjanSCC() {
+    sccId = discovered = vector<int>(adj.size(), -1);
+    sccCounter = vectexCounter = 0;
+
+    for (int i = 0; i < adj.size(); i++)
+        if (discovered[i] == -1)
+            scc(i);
+
+    return sccId;
+}
