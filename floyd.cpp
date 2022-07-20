96aa77efd5719d2aba0cb6940948cc68dedb0645
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-07-20 17:00:51 +0900

    Create floyd.cpp

---
+++ floyd.cpp
@@ -0,0 +1,12 @@
+int V;
+int adj[MAX_V][MAX_V];
+
+void floyd() {
+    for (int i = 0; i < V; i++)
+        adj[i][i] = 0;
+
+    for (int k = 0; k < V; k++)
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+}
